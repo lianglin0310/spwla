66a54b95b20781d2468d519ce5c96b2541711d1c
lianglin0310 <lianglin0310@yahoo.com>
2020-06-04 01:39:12 -0400

    add unit test for resistivity archie

--- tests/test_spwla.py
+++ tests/test_spwla.py
@@ -1,6 +1,10 @@
 
 from spwla.cli import main
+from spwla.resistivity import archie
 
 
 def test_main():
     assert main([]) == 0
+
+def test_archie():
+    assert archie(1.0, 0.2, 2, 2, 1) == 0.04
